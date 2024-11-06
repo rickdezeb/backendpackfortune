0de25984ea5f78ea2c21459f019a181116bf5e8e
rickdezeb <xrick19@gmail.com>
2024-11-06 15:20:00 +0100

    changed docker file
    changed docker file

--- Packfortune.API/Program.cs
+++ Packfortune.API/Program.cs
@@ -54,6 +54,11 @@ builder.Services.AddRateLimiter(options =>
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
